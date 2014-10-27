1493366263048b179339f3a093bd8e4e68800a85
William Wedler <wwedler@riseup.net>
2014-10-27 19:42:09 -0400

    cleaning up routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ BikeBike::Application.routes.draw do
 	#resources :workshop_facilitators
 	#resources :registration_form_fields
 
-#  get ':controller/:conference_type/:conference_slug', controller: /conferences\/[^\/]+/, action: :show
-
   resources :conferences, :only => :index
   resources :conferences, :param => :slug, :path => 'conferences/:conference_type' do
 
@@ -19,49 +17,20 @@ BikeBike::Application.routes.draw do
     get 'registrations' => 'conferences#registrations'
   end
 
+  resources :organizations, :param => 'slug' do
+    get :members
+    get :identity
+    get :json
+    post :nonmembers
+  end
 
+  resources :users, :except => [:new, :index, :destroy] 
+  get	'register', to: 'users#new', :as => 'register'
 
+  resources :user_sessions, :only => [:create]
+  get	'login', to: 'user_sessions#new', :as => :login
+  post	'logout', to: 'user_sessions#destroy', :as => :logout
 
-#	resources :conference_types, :param => :type, :path => '/conferences', :as => :conference, :except => :index do
-#		resources :conferences, :param => :slug, :path => '/' do
-			#get :hosts
-			#get :registration
-			#get :registration
-            #resources :workshops, :param => 'slug'
-			#get :register, :param => 'step'
-            #post 'register/next' => 'conferences#register_submit'
-            # get 'register/workshop-test/' => 'conferences#workshop_test'
-
-
-
-
-
-            #patch 'register/step/:step' => 'conferences#register_step'
-			#resources :registrations, :path => 'registration' do
-			#	get :form, on: :collection
-			#end
-			#get 'registration/form' => 'conferences#registration', :sub_action => "form", as: 'registration_form'
-			#get 'registration/form/register' => 'conferences#registration', :sub_action => "register", as: 'registration_register'
-			#get 'registration/form/stats' => 'conferences#registration', :sub_action => "stats", as: 'registration_stats'
-			#post :nonhosts
-			#post 'registration/form/add-field' => 'conferences#add_field', as: 'registration_add_field'
-			#post 'registration/form/remove-field' => 'conferences#remove_field', as: 'registration_remove_field'
-            #post 'registration/form/reorder' => 'conferences#reorder', as: 'registration_reorder'
-			
-            #post 'registration/form/reorder' => 'conferences#reorder', as: 'registration_reorder'
-#		end
-#	end
-
-
-	resources :organizations, :param => 'slug' do
-		get :members
-		get :identity
-		get :json
-		post :nonmembers
-	end
-
-	resources :users, :except => [:list, :destroy]
-	resources :user_sessions, :only => [:new, :create, :destroy]
 
 
 	#resources :workshop_streams
@@ -76,9 +45,6 @@ BikeBike::Application.routes.draw do
 	get '/translations/:lang', to: 'pages#translations'
 	get '/translations', to: 'pages#translation_list'
 
-	get		'login' => 'user_sessions#new', :as => :login
-	post	'logout' => 'user_sessions#destroy', :as => :logout
-	get		'register'  => 'users#new', :as => 'register'
   
 	post "oauth/callback" => "oauths#callback"
 	get "oauth/callback" => "oauths#callback"
